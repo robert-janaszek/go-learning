51a03e9441e0cb0452a0b73d3f8abba73d7a826d
Robert Janaszek <robert.janaszek@gmail.com>
2026-07-24 16:05:31 +0200

    Add functions for safe division, incrementing integers, and local variable allocation in day1b.go; include examples demonstrating their usage.

--- day1b.go
+++ day1b.go
@@ -23,6 +23,40 @@ func uppercaseMutate(str *string) {
 	*str = strings.ToUpper(*str)
 }
 
+func safeDivide(a, b int, result *float64) bool {
+	if b == 0 {
+		return false
+	}
+
+	*result = float64(a) / float64(b)
+
+	return true
+}
+
+func safeDivide2(a, b int) (float64, bool) {
+	if b == 0 {
+		return 0, false
+	}
+
+	return float64(a) / float64(b), true
+}
+
+func increment(a *int) {
+	*a++
+}
+
+func allocateInt(a int) *int {
+	localVal := a
+
+	return &localVal
+}
+
+func localInt(a int) int {
+	localVal := a
+
+	return localVal
+}
+
 func day1b() {
 	// ex 11
 	a := 1
@@ -43,4 +77,59 @@ func day1b() {
 	uppercaseMutate(&str)
 
 	fmt.Println(str, newStr)
+
+	// ex 13
+
+	a1 := 12
+	b1 := 6
+	var result float64
+	c1 := 0
+
+	done := safeDivide(a1, b1, &result)
+
+	fmt.Println(done, result) // true 2
+
+	done = safeDivide(a1, c1, &result)
+
+	fmt.Println(done, result) // false 2
+
+	result, done = safeDivide2(a1, 3)
+
+	fmt.Println(done, result)
+
+	// ex 14
+	a2 := 0
+
+	for i := 0; i < 3; i++ {
+		increment(&a2)
+	}
+
+	fmt.Println(a2)
+
+	a2 = 0
+
+	for range 3 {
+		increment(&a2)
+	}
+
+	a2 = 0
+
+	for i := range 3 {
+		increment(&a2)
+		fmt.Print(i)
+	}
+
+	fmt.Println()
+	fmt.Println(a2)
+
+	// ex 15
+	a3 := 12
+
+	result3 := allocateInt(a3)
+
+	fmt.Println(*result3)
+
+	outsideLocal := localInt(a3)
+
+	fmt.Println(outsideLocal)
 }
