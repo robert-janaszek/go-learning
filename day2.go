d962c4c5f0803d01d25bac4baf9bec78ba09e089
Robert Janaszek <robert.janaszek@gmail.com>
2026-07-25 08:51:08 +0200

    Add methods for Book and Celsius types in day2.go; implement Summary and MarkAsRead for Book, and conversion and addition methods for Celsius.

--- day2.go
+++ day2.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Book struct {
 	Title  string
 	Author string
 	Pages  int
 	IsRead bool
+}
+
+func (b Book) Summary() string {
+	// return "\"" + b.Title + "\" by " + b.Author + " (" + strconv.Itoa(b.Pages) + " pages)"
+	return fmt.Sprintf("\"%s\" by %s (%d pages)", b.Title, b.Author, b.Pages)
+}
+
+func (b *Book) MarkAsRead() {
+	b.IsRead = true
 }
 
 type BookWithTags struct {
@@ -23,6 +34,17 @@ func NewBook(title string, author string, pages int) *Book {
 		Author: author,
 		Pages:  pages,
 	}
+}
+
+type Celsius float64
+
+func (c Celsius) ToFahrenheit() float64 {
+	return float64(c)*9/5 + 32
+}
+
+func (c *Celsius) Add(value float64) {
+	// *c = Celsius(float64(*c) + value) -- ok
+	*c += Celsius(value)
 }
 
 func day2() {
@@ -90,7 +112,28 @@ func day2() {
 	book6a := BookWithTags{}
 	book6b := BookWithTags{}
 
-	fmt.Print(book6a, book6b)
+	fmt.Println(book6a, book6b)
 
 	// fmt.Println(book6a == book6b) // Do not compile
+
+	// ex 6
+	fmt.Println(book.Summary())
+
+	// ex 7
+	book2.MarkAsRead()
+	fmt.Printf("%+v\n", book2)
+
+	// ex 8
+	book7 := Book{Title: "Go in Action"}
+	book7.MarkAsRead()
+	// (&book7).MarkAsRead() - not needed
+	fmt.Printf("%+v\n", book7)
+
+	// ex 9
+	temp := Celsius(100)
+	fmt.Println(temp.ToFahrenheit())
+
+	// ex 10
+	temp.Add(10)
+	fmt.Println(temp)
 }
