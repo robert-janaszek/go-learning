c42215f429a193132ba0b2ea88ccd2dcdfab6bb7
Robert Janaszek <robert.janaszek@gmail.com>
2026-07-28 14:48:45 +0300

    Add namedBook struct and enhance stringer interface: implement String method for namedBook, demonstrate usage in printInfo function, and showcase handling of nil stringer in Day3a function.

--- course/day3a.go
+++ course/day3a.go
@@ -11,11 +11,25 @@ type namedUser struct {
 	Age  int
 }
 
+type namedBook struct {
+	Title  string
+	Author string
+}
+
 func (u namedUser) String() string {
 	return fmt.Sprintf("%s, %d", u.Name, u.Age)
 }
 
+func (b namedBook) String() string {
+	return fmt.Sprintf("\"%s\" by %s", b.Title, b.Author)
+}
+
 var _ stringer = namedUser{}
+var _ stringer = namedBook{}
+
+func printInfo(s stringer) {
+	fmt.Println(s.String())
+}
 
 func Day3a() {
 	// ex 1
@@ -25,4 +39,30 @@ func Day3a() {
 	}
 
 	fmt.Println(mark.String())
+
+	// ex 2
+	printInfo(mark)
+
+	// ex 3
+
+	orbium := namedBook{
+		Title:  "De revolutionibus orbium coelestium",
+		Author: "Nicolas Copernicus",
+	}
+
+	printInfo(orbium)
+
+	// ex 4
+
+	var s stringer
+	if s == nil {
+		fmt.Println("s is nil")
+		// s.String() -- panic: runtime error: invalid memory address or nil pointer dereference
+	}
+
+	// ex 5
+	items := []stringer{mark, orbium}
+	for _, item := range items {
+		fmt.Println(item.String())
+	}
 }
