4c24011a258ef21e67c1b838bcd718a407f0b01b
Robert Janaszek <robert.janaszek@gmail.com>
2026-07-28 14:37:42 +0300

    Add namedUser struct and stringer interface: implement String method for namedUser to format user details, and demonstrate usage in Day3a function.

--- course/day3a.go
+++ course/day3a.go
@@ -1,5 +1,28 @@
 package course
 
-func Day3a() {
+import "fmt"
 
+type stringer interface {
+	String() string
+}
+
+type namedUser struct {
+	Name string
+	Age  int
+}
+
+func (u namedUser) String() string {
+	return fmt.Sprintf("%s, %d", u.Name, u.Age)
+}
+
+var _ stringer = namedUser{}
+
+func Day3a() {
+	// ex 1
+	mark := namedUser{
+		Name: "Mark",
+		Age:  32,
+	}
+
+	fmt.Println(mark.String())
 }
