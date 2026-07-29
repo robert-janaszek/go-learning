f42a26e5446739a1fd949e2ef755d4b596aefdb3
Robert Janaszek <robert.janaszek@gmail.com>
2026-07-29 09:09:19 +0300

    Add Day 4a implementation: introduce age validation, order processing, and user finding functions with error handling examples, demonstrating practical usage in the Day4a function.

---
+++ course/day4a.go
@@ -0,0 +1,82 @@
+package course
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func ValidateAge(age int) error {
+	if age < 0 || age > 120 {
+		return fmt.Errorf("age %d is out of range [0-120]", age)
+	}
+
+	return nil
+}
+
+func ProcessOrder(id int, amount float64) error {
+	if id <= 0 {
+		return fmt.Errorf("id %d cannot be 0 or lower", id)
+	}
+
+	if amount <= 0 {
+		return fmt.Errorf("amount %v cannot be 0 or lower", amount)
+	}
+
+	if amount >= 10000 {
+		return fmt.Errorf("amount %v cannot be 10000 or higher", amount)
+	}
+
+	return nil
+}
+
+var ErrNotFound = errors.New("item not found")
+var ErrPermissionDenied = errors.New("permission denied")
+
+func FindUser(id int) (*user, error) {
+	if id == 0 {
+		return nil, ErrNotFound
+	}
+
+	return nil, nil
+}
+
+func Day4a() {
+	// ex 1, 2
+	ages := []int{10, -1, 121}
+
+	for _, age := range ages {
+		err := ValidateAge(age)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	// ex 3
+
+	err1 := ProcessOrder(0, 1000)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	err2 := ProcessOrder(1, 0)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	err3 := ProcessOrder(2, 1000)
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+
+	// ex4
+
+	_, err4 := FindUser(0)
+
+	if err4 == ErrNotFound {
+		fmt.Println(err4)
+	}
+
+	// ex5
+
+	val, _ := strconv.Atoi("123")
+	fmt.Println(val)
+}
