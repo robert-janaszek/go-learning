529f9c0f179b81062fb94f9e9aad06953f98a1ae
Robert Janaszek <robert.janaszek@gmail.com>
2026-08-05 15:17:53 +0200

    feat(day8): implement command-line flags for task management operations

--- taskmanager/main.go
+++ taskmanager/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"taskmanager/storage"
 	"taskmanager/task"
 )
 
 func main() {
-	js := storage.NewJSONStorage("test.json")
 
+	flagAdd := flag.String("add", "", "-add \"Buy milk\"")
+	flagList := flag.Bool("list", false, "-list, -list -all")
+	flagAll := flag.Bool("all", false, "-list -all")
+	flagDone := flag.Int("done", -1, "-done 1")
+
+	flag.Parse()
+
+	js := storage.NewJSONStorage("test.json")
 	manager, err := task.NewManager(js)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("%v\n", err)
 	}
 
-	err = manager.MarkDone(1)
+	switch {
+	case *flagAdd != "":
+		err := manager.Add(*flagAdd)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+	case *flagList:
+		fmt.Println(manager.List(*flagAll))
+	case *flagDone != -1:
+		err := manager.MarkDone(*flagDone)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
 
-	if err != nil {
-		fmt.Println(err)
-		return
+	default:
+		flag.Usage()
 	}
-
-	fmt.Println(manager.List(false))
 }
